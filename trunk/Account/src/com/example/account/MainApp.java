7bb5789676cdc3a297a9688d6ad27fc82cfec278
poolsawatapin@gmail.com <poolsawatapin@gmail.com@00a0f6ac-e8cf-8ae8-f0c9-6a34a4de0bea>
2014-05-10 12:22:26 +0000

    -Create Menu Action - query list
    10/05/2557
    19.22
    By PoolSawat

--- trunk/Account/src/com/example/account/MainApp.java
+++ trunk/Account/src/com/example/account/MainApp.java
@@ -1,10 +1,16 @@
 package com.example.account;
 
-import android.app.Activity;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 import android.app.AlertDialog;
-import android.content.Context;
+import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.PopupMenu.OnMenuItemClickListener;
@@ -16,23 +22,28 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.CalendarView.OnDateChangeListener;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SimpleAdapter;
 import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainApp extends ActionBarActivity {
-	private String[] itemMenu = {"Add","View","Help","Exit"};
-	
-	CalendarView calendar;
-	String msg = "";
-
+	private String[] itemMenu = { "Add", "View", "Help", "Exit" };
+	ListView list_tran;
+	MyDbHelper dbHelper;
+	SQLiteDatabase db;
+	Cursor cursor;
+	SimpleCursorAdapter adapter;
 	/*
 	 * Component
 	 */
+	CalendarView calendar;
 	RadioGroup groupType;
 	EditText txtName;
 	EditText txtDetail;
@@ -43,6 +54,8 @@ public class MainApp extends ActionBarActivity {
 		txtName = (EditText) findViewById(R.id.txt_name);
 		txtDetail = (EditText) findViewById(R.id.txt_detail);
 		txtValue = (EditText) findViewById(R.id.txt_value);
+
+		list_tran = (ListView) findViewById(R.id.list_transaction);
 	}
 
 	@Override
@@ -51,36 +64,43 @@ public class MainApp extends ActionBarActivity {
 		setContentView(R.layout.activity_calendar);
 		findByIdComponent();
 		calendar = (CalendarView) findViewById(R.id.calendar);
-		calendar.setOnDateChangeListener(this.calendarListener);	
+		calendar.setOnDateChangeListener(this.calendarListener);
 		registerForContextMenu(calendar);
 		dialogForm();
+
+		dbHelper = new MyDbHelper(this);
 	}
+
 	@Override
-	public void onCreateContextMenu(ContextMenu menu,View v,ContextMenuInfo menuInfo){
-			AdapterView.AdapterContextMenuInfo info = 
-					(AdapterView.AdapterContextMenuInfo)menuInfo;
-			menu.setHeaderTitle(itemMenu[info.position]);
-			for (int i = 0; i < itemMenu.length; i++) {
-				menu.add(Menu.NONE,i,i,itemMenu[i]);
-			}
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+		menu.setHeaderTitle(itemMenu[info.position]);
+		for (int i = 0; i < itemMenu.length; i++) {
+			menu.add(Menu.NONE, i, i, itemMenu[i]);
+		}
 	}
+
 	@Override
-	public boolean onContextItemSelected(MenuItem item){
-		
+	public boolean onContextItemSelected(MenuItem item) {
+
 		return true;
 	}
+
 	public OnDateChangeListener calendarListener = new OnDateChangeListener() {
 
 		@Override
 		public void onSelectedDayChange(CalendarView view, int year, int month,
 				int dayOfMonth) {
-			PopupMenu popup = new PopupMenu(MainApp.this, view);
+			/*PopupMenu popup = new PopupMenu(MainApp.this, view);
 			popup.setOnMenuItemClickListener(itemClick);
 
 			MenuInflater inflater = popup.getMenuInflater();
 			inflater.inflate(R.menu.popup_menu, popup.getMenu());
 
-			popup.show();
+			popup.show();*/
+			
+			dialogMenu().show();
 
 			/*
 			 * Toast.makeText(MainApp.this, "day : "+dayOfMonth+"\n"
@@ -88,6 +108,31 @@ public class MainApp extends ActionBarActivity {
 			 */
 		}
 	};
+	private AlertDialog.Builder dialogMenu(){
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Select Function");
+		builder.setItems(itemMenu, new DialogInterface.OnClickListener() {			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				switch (itemMenu[which]) {
+				case "Add":		
+					dialogForm().show();
+					break;
+				case "View":
+					dialogListItem().show();
+					break;
+				case "Help":					
+					break;
+				case "Exit":					
+					break;
+				default:
+					System.out.println("default==>>");
+					break;
+				}
+			}
+		});
+		return builder;
+	}
 	private OnMenuItemClickListener itemClick = new OnMenuItemClickListener() {
 
 		@Override
@@ -99,8 +144,12 @@ public class MainApp extends ActionBarActivity {
 				dialogForm().show();
 				return true;
 			case R.id.action_view:
-				Toast.makeText(MainApp.this, "action_view", Toast.LENGTH_SHORT)
-						.show();
+				/*
+				 * Toast.makeText(MainApp.this, "action_view",
+				 * Toast.LENGTH_SHORT) .show();
+				 */
+				loadListItem();
+				dialogListItem().show();
 				return true;
 			default:
 				return false;
@@ -122,15 +171,14 @@ public class MainApp extends ActionBarActivity {
 				// int index =
 				// groupType.indexOfChild(findViewById(groupType.getCheckedRadioButtonId()));
 				try {
-					Toast.makeText(
-							MainApp.this,
-							"Type: " 
-									+ "\n Name: " //+ txtName.getText()
-									+ "\n Detail: " //+ txtDetail.getText()
-									+ "\n Number: " //+ txtValue.getText()
-							,Toast.LENGTH_LONG).show();
+					Toast.makeText(MainApp.this, "Type: " + "\n Name: " // +
+																		// txtName.getText()
+							+ "\n Detail: " // + txtDetail.getText()
+							+ "\n Number: " // + txtValue.getText()
+					, Toast.LENGTH_LONG).show();
 				} catch (Exception e) {
-					Toast.makeText(MainApp.this, e.toString(), Toast.LENGTH_LONG).show();
+					Toast.makeText(MainApp.this, e.toString(),
+							Toast.LENGTH_LONG).show();
 				}
 			}
 		});
@@ -143,5 +191,117 @@ public class MainApp extends ActionBarActivity {
 					}
 				});
 		return builder;
+	}
+
+	private AlertDialog.Builder dialogListItem() {
+
+		LayoutInflater inflater = MainApp.this.getLayoutInflater();
+		AlertDialog.Builder builder = new AlertDialog.Builder(MainApp.this);
+		builder.setView(inflater.inflate(R.layout.list_transaction, null));
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface arg0, int arg1) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		builder.setNegativeButton("CANCLE",
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+		return builder;
+	}
+
+	private Account getValue() {
+		Account account = new Account();
+
+		return account;
+	}
+
+	private void loadListItem() {
+		// ชื่อ field ที่จะ query ข้อมูล
+		/*
+		 * String[] queryColumnRev = new String[] {"_id",
+		 * MyDbHelper.COL_TRAN_NAME, MyDbHelper.COL_TRAN_DATE,
+		 * MyDbHelper.COL_TRAN_DETAIL, MyDbHelper.COL_TRAN_TYPE,
+		 * MyDbHelper.COL_TRAN_VALUE}; cursor =
+		 * db.query(MyDbHelper.TABLE_TRANSAC, queryColumnRev,
+		 * null,null,null,null,null);
+		 * 
+		 * String[] showColumnRev = new String[] { MyDbHelper.COL_TRAN_NAME,
+		 * MyDbHelper.COL_TRAN_TYPE, MyDbHelper.COL_TRAN_DETAIL,
+		 * MyDbHelper.COL_TRAN_DATE, MyDbHelper.COL_TRAN_VALUE };
+		 * 
+		 * int[] viewsRev = new int[] { android.R.id.text1, android.R.id.text2
+		 * };
+		 * 
+		 * this.adapter = new SimpleCursorAdapter(this, R.layout.listitem_data,
+		 * cursor, showColumnRev, viewsRev); list_tran.setAdapter(adapter);
+		 */
+
+		String sql = "SELECT * FROM transac";
+
+		cursor = db.rawQuery(sql, null);
+
+		ArrayList data = new ArrayList();		
+		int item = android.R.layout.simple_list_item_1;
+		while (cursor.moveToNext()) {
+			
+			String name = cursor.getString(cursor
+					.getColumnIndex(MyDbHelper.COL_TRAN_NAME));
+			String detail = cursor.getString(cursor
+					.getColumnIndex(MyDbHelper.COL_TRAN_DETAIL));
+			String date = cursor.getString(cursor
+					.getColumnIndex(MyDbHelper.COL_TRAN_DATE));
+			String type = cursor.getString(cursor
+					.getColumnIndex(MyDbHelper.COL_TRAN_TYPE));
+			String value = cursor.getString(cursor
+					.getColumnIndex(MyDbHelper.COL_TRAN_VALUE));
+			
+			HashMap map = new HashMap();
+				map.put(MyDbHelper.COL_TRAN_NAME, name);
+				map.put(MyDbHelper.COL_TRAN_DETAIL,detail);
+				map.put(MyDbHelper.COL_TRAN_DATE, date);
+				map.put(MyDbHelper.COL_TRAN_TYPE, type);
+				map.put(MyDbHelper.COL_TRAN_VALUE, value);						
+			data.add(map);
+		}
+		System.out.println("data.size: "+data.size());
+		
+		String[] column = new String[] { MyDbHelper.COL_TRAN_NAME,
+				MyDbHelper.COL_TRAN_TYPE, MyDbHelper.COL_TRAN_DETAIL,
+				MyDbHelper.COL_TRAN_DATE, MyDbHelper.COL_TRAN_VALUE };
+		int[] value = new int[] {
+				R.id.txt_result_name,R.id.txt_result_type,
+				R.id.txt_result_detail,R.id.txt_result_date,
+				R.id.txt_result_value
+		};
+		
+		int itemData = R.layout.listitem_data;
+		
+		SimpleAdapter adapter = 
+				new SimpleAdapter(this, data, itemData, column, value);
+		
+		list_tran.setAdapter(adapter);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		// เข้าถึงฐานข้อมูลเพื่ออ่านและเขียน
+		db = dbHelper.getWritableDatabase();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		cursor.close();
+		db.close();
 	}
 }
--- trunk/Account/src/com/example/account/MyDbHelper.java
+++ trunk/Account/src/com/example/account/MyDbHelper.java
@@ -5,13 +5,15 @@ import java.util.Date;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MyDbHelper extends SQLiteOpenHelper{
 	private static final String DB_NAME = "mydb";
 	private static final int DB_VERSION = 1;
+	private static final String typeRev = "revenue",typeExp = "expenses";
+	
 	public static final String TABLE_NAME = "people";
 	public static final String COL_NAME = "pName";
 	public static final String COL_DATE = "pDate";
@@ -19,60 +21,65 @@ public class MyDbHelper extends SQLiteOpenHelper{
 	/*
 	 * database account
 	 */
-	public static final String TABLE_REVENUE = "revenue";
-	public static final String COL_REV_ID = "rId";
-	public static final String COL_REV_DATE = "rDate";
-	public static final String COL_REV_NAME = "rName";
-	public static final String COL_REV_DETAIL = "rDetail";
-	public static final String COL_REV_VALUE = "rValue";
-	
-	public static final String TABLE_EXPENSES = "expenses";
-	public static final String COL_EXP_ID = "eId";
-	public static final String COL_EXP_DATE = "eDate";
-	public static final String COL_EXP_NAME = "eName";
-	public static final String COL_EXP_DETAIL = "eDetail";
-	public static final String COL_EXP_VALUE = "eValue";
+	public static final String TABLE_TRANSAC = "transac";
+	public static final String COL_TRAN_ID = "tId";
+	public static final String COL_TRAN_DATE = "tDate";
+	public static final String COL_TRAN_NAME = "tName";
+	public static final String COL_TRAN_DETAIL = "tDetail";
+	public static final String COL_TRAN_TYPE = "tType";
+	public static final String COL_TRAN_VALUE = "tValue";
 	
 	private static final String STRING_CREATE = 
 			"CREATE TABLE "+ TABLE_NAME
 			+ " (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
 			+ COL_NAME+ " TEXT ," + COL_DATE + " DATE);";
-	private static final String STRING_CREATE_REV = 
-			"CREATE TABLE "+TABLE_REVENUE
-			+ " (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-			+ COL_REV_NAME + "VARCHAR(50) ,"
-			+ COL_REV_DATE + "DATE ,"
-			+ COL_REV_DETAIL + "TEXT ,"
-			+ COL_REV_VALUE + "INTEGER(10));";
 	
-	private static final String STRING_CREATE_EXP = 
-			"CREATE TABLE "+TABLE_EXPENSES
-			+ " (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-			+ COL_EXP_NAME + " VARCHAR(50) ,"
-			+ COL_EXP_DATE + "DATE ,"
-			+ COL_EXP_DETAIL + "TEXT ,"
-			+ COL_EXP_VALUE + "INTEGER(10));";
+	private static final String STRING_CREATE_TRANSAC = 
+			"CREATE TABLE "+ TABLE_TRANSAC
+			+ " (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+			+ COL_TRAN_NAME + " TEXT ,"
+			+ COL_TRAN_DATE + " DATE ,"
+			+ COL_TRAN_DETAIL + " TEXT ,"
+			+ COL_TRAN_TYPE + " TEXT ,"
+			+ COL_TRAN_VALUE + " TEXT );";
 	
 	public MyDbHelper(Context context) {
 		super(context, DB_NAME, null, DB_VERSION);
+		System.out.println("sql STRING_CREATE_TRANSAC: "+STRING_CREATE_TRANSAC);
+		System.out.println("sql STRING_CREATE: "+STRING_CREATE);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL(STRING_CREATE);
 		
-		ContentValues cv = new ContentValues();
-		
-		cv.put(COL_NAME, "Promlert Lovichit");
-		
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		
-		cv.put(COL_DATE, dateFormat.format(new Date()));
-		
-		db.insert(TABLE_NAME, null, cv);
-		
-		db.execSQL(STRING_CREATE_EXP);
-		db.execSQL(STRING_CREATE_REV);
+		try {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			
+			db.execSQL(STRING_CREATE);
+			
+			ContentValues cv = new ContentValues();		
+			cv.put(COL_NAME, "Promlert Lovichit");				
+			cv.put(COL_DATE, dateFormat.format(new Date()));
+			
+			db.insert(TABLE_NAME, null, cv);
+			
+			/*
+			 * 
+			 */
+			
+			db.execSQL(STRING_CREATE_TRANSAC);
+			
+			ContentValues cvTran = new ContentValues();
+			cvTran.put(COL_TRAN_NAME, "mama");
+			cvTran.put(COL_TRAN_DETAIL, "wiwi");
+			cvTran.put(COL_TRAN_DATE, dateFormat.format(new Date()));
+			cvTran.put(COL_TRAN_TYPE, typeRev);
+			cvTran.put(COL_TRAN_VALUE, 500);
+			
+			db.insert(TABLE_TRANSAC, null, cvTran);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
@@ -80,6 +87,9 @@ public class MyDbHelper extends SQLiteOpenHelper{
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
 			
+		onCreate(db);
+		
+		db.execSQL("DROP TABLE IF EXISTS "+TABLE_TRANSAC);
 		onCreate(db);
 		
 		if (oldVersion <= 1) {
